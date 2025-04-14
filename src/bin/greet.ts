e32e17e60996711ef21d4487d6a68cbebced665b
Oren Levi <oren.stu@gamil.com>
2025-04-14 21:31:30 +0300

    add basic static proxy

--- src/bin/greet.ts
+++
@@ -1,5 +0,0 @@
-import "zx/globals";
-
-const name = await question("What is your name? ");
-
-echo`Hello, ${name}!\n`;
---
+++ src/bin/proxy.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import { createProxyMiddleware } from "http-proxy-middleware";
+
+const port = 8080;
+
+const server = express();
+
+server.use(
+  createProxyMiddleware({
+    pathFilter: "/wttr",
+    target: "http://wttr.in",
+    changeOrigin: true,
+  }),
+);
+
+server.listen(port, () => {
+  console.log(`server listening on ${port}`);
+});
